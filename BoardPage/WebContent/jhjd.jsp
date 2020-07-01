5b2505ec86feed5ed5f003b57edd3692cf770f6d
Donghyeon Kim <kevink3@naver.com>
2020-07-01 21:12:02 +0900

    크롤링, 쿼리 수정

--- BoardPage/WebContent/jhjd.jsp
+++ BoardPage/WebContent/jhjd.jsp
@@ -1,11 +1,11 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%-- <%@ page import="org.openqa.selenium.By" %>
+<%@ page import="org.openqa.selenium.By" %>
 <%@ page import="org.openqa.selenium.WebDriver" %>
 <%@ page import="org.openqa.selenium.chrome.ChromeDriver" %>
 <%@ page import="org.openqa.selenium.chrome.ChromeOptions" %>
- --%>
- <%@ page import="selenium.selenium" %>
+ 
+<%--  <%@ page import="selenium.selenium" %> --%>
  
  
  <html>
@@ -16,10 +16,19 @@
  </head>
  <body>
  
-	<% 
-		selenium test = new selenium();
-	
+	<%! 
+		private WebDriver driver;
+	 	public static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
+	    public static final String WEB_DRIVER_PATH = "./chromedriver.exe";
+	    
+	    //크롤링 할 URL
+	    private String base_url;
 	%> 
+	
+	<%
+	
+		
+	%>
  </body>
 	
  
--- BoardPage/src/selenium/selenium.java
+++ BoardPage/src/selenium/selenium.java
@@ -11,36 +11,27 @@ private WebDriver driver;
     //Properties
     public static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
     public static final String WEB_DRIVER_PATH = "./chromedriver.exe";
-    
     //크롤링 할 URL
     private String base_url;
     
     public selenium() {
         super();
- 
         //System Property SetUp
         System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
-        
         //Driver SetUp
         driver = new ChromeDriver();
-        base_url = "https://www.naver.com";
+        base_url = "https://www.kosaf.go.kr/ko/notice.do?ctgrId1=&ctgrId2=&searchStr=&searchType=&page=1&pg=";
     }
  
     public void crawl() {
- 
         try {
             //get page (= 브라우저에서 url을 주소창에 넣은 후 request 한 것과 같다)
             driver.get(base_url);
             System.out.println(driver.getPageSource());
-    
         } catch (Exception e) {
-            
             e.printStackTrace();
-        
         } finally {
- 
             driver.close();
         }
- 
     }
 }
